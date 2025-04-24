d28a0a6e799b6f373ad5e8422a0746ba758eda97
MouliMohanN <moulimohann@gmail.com>
2025-04-24 13:44:42 +0530

    Button template

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,9 +1,12 @@
 import {Text, View} from 'react-native';
+import { Button } from '../common/components/Button';
 
 export const MainScreen = () => {
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={{flex: 1, alignItems: 'center', padding: 16}}>
       <Text>Main Screen</Text>
+      <Button title='Go to screen' onPress={() => { }} />
     </View>
   );
 };
