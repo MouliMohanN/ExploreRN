47e3aeafa98ea7eb72f9ad5d72fb2374c7bf4099
MouliMohanN <moulimohann@gmail.com>
2025-07-24 10:14:34 +0530

    TabViewReanimated example and package upgradation

--- src/features/tabView/CounterContext.tsx
+++ src/features/tabView/CounterContext.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 
-export const CounterContext = React.createContext<{ count: number; setCount: (count: number) => void; }>({ count: 0, setCount: () => {} });
+export const CounterContext = React.createContext<{ count: number; setCount: (count: number) => void }>({
+  count: 0,
+  setCount: () => {},
+});
 
 export const useCounter = () => {
   const [count, setCount] = useState(0);
--- src/features/tabView/TabViewReanimatedScreen.tsx
+++ src/features/tabView/TabViewReanimatedScreen.tsx
@@ -1,11 +1,50 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { ScreenBaseProps } from '../../common/types/ScreenBaseProps';
+import { Dimensions, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import * as TabView from 'reanimated-tab-view';
+import { CounterContext, useCounter } from './CounterContext';
+import { TabViewScreen1 } from './screens/TabViewScreen1';
+import { TabViewScreen2 } from './screens/TabViewScreen2';
 
-export const TabViewReanimatedScreen: React.FC<ScreenBaseProps> = ({}) => {
+const tabs = [
+  { key: 'screen1', title: 'Screen 1', component: TabViewScreen1 },
+  { key: 'screen2', title: 'Screen 2', component: TabViewScreen2 },
+  { key: 'screen3', title: 'Screen 3', component: TabViewScreen1 },
+  { key: 'screen4', title: 'Screen 4', component: TabViewScreen2 },
+  { key: 'screen5', title: 'Screen 5', component: TabViewScreen1 },
+  { key: 'screen6', title: 'Screen 6', component: TabViewScreen2 },
+];
+
+export function TabViewReanimatedScreen() {
+  const [index, setIndex] = React.useState(0);
+  const { count, setCount } = useCounter();
+
+  const routes = tabs.map((tab) => ({ key: tab.key, title: tab.title }));
+
+  const renderScene = ({ route }: { route: { key: string } }) => {
+    const tab = tabs.find((t) => t.key === route.key);
+    if (tab) {
+      const Component = tab.component;
+      return (
+        <View style={{ flex: 1, backgroundColor: 'white', marginTop: 80 }}>
+          <Component />
+        </View>
+      );
+    }
+    return null;
+  };
+
   return (
-    <View>
-      <Text>TabViewReanimatedScreen</Text>
-    </View>
+    <GestureHandlerRootView>
+      <CounterContext.Provider value={{ count, setCount }}>
+        <TabView.TabView
+          navigationState={{ index, routes }}
+          renderScene={renderScene}
+          onIndexChange={setIndex}
+          initialLayout={{ width: Dimensions.get('window').width }}
+          renderTabBar={(props) => <TabView.TabBar {...props} scrollEnabled />}
+        />
+      </CounterContext.Provider>
+    </GestureHandlerRootView>
   );
-};
+}
