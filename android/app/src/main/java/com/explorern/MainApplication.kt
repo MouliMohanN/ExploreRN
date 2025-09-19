b0c7303f4841284a716df7cf2218a4aa6ff97d68
Mouli Mohan N <moulimohann@Moulis-MacBook-Pro.local>
2025-09-19 11:41:36 +0530

    brotli integration

--- android/app/src/main/java/com/explorern/MainApplication.kt
+++ android/app/src/main/java/com/explorern/MainApplication.kt
@@ -12,6 +12,10 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 import com.explorern.nativeModules.NetworkBrotliPackage
+import okhttp3.OkHttpClient
+import okhttp3.brotli.BrotliInterceptor
+import com.facebook.react.modules.network.OkHttpClientProvider
+import com.facebook.react.modules.network.OkHttpClientFactory
 
 class MainApplication : Application(), ReactApplication {
 
@@ -37,6 +41,16 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
+    
+    // Configure OkHttp with Brotli compression
+    OkHttpClientProvider.setOkHttpClientFactory(object : OkHttpClientFactory {
+      override fun createNewNetworkModuleClient(): OkHttpClient {
+        return OkHttpClientProvider.createClientBuilder()
+          .addInterceptor(BrotliInterceptor)
+          .build()
+      }
+    })
+    
     SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
--- android/app/src/main/java/com/explorern/nativeModules/NetworkBrotliModule.kt
+++ android/app/src/main/java/com/explorern/nativeModules/NetworkBrotliModule.kt
@@ -5,25 +5,40 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.module.annotations.ReactModule
+import com.facebook.react.modules.network.OkHttpClientProvider
+import com.facebook.react.modules.network.OkHttpClientFactory
+import okhttp3.OkHttpClient
+import okhttp3.brotli.BrotliInterceptor
 
 @ReactModule(name = NetworkBrotliModule.NAME)
 class NetworkBrotliModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
     companion object {
         const val NAME: String = "NetworkBrotli"
+        private var brotliEnabled: Boolean = true // Default enabled since we set it up in MainApplication
     }
-
-    private var enabled: Boolean = false
 
     override fun getName(): String = NAME
 
     @ReactMethod
     fun isEnabled(promise: Promise) {
-        promise.resolve(enabled)
+        promise.resolve(brotliEnabled)
     }
 
     @ReactMethod
     fun setEnabled(enabled: Boolean, promise: Promise) {
-        this.enabled = enabled
-        promise.resolve(enabled)
+        brotliEnabled = enabled
+        
+        // Reconfigure OkHttpClient based on enabled state
+        OkHttpClientProvider.setOkHttpClientFactory(object : OkHttpClientFactory {
+            override fun createNewNetworkModuleClient(): OkHttpClient {
+                val builder = OkHttpClientProvider.createClientBuilder()
+                if (brotliEnabled) {
+                    builder.addInterceptor(BrotliInterceptor)
+                }
+                return builder.build()
+            }
+        })
+        
+        promise.resolve(brotliEnabled)
     }
 }
