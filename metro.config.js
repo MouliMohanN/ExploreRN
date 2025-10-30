7a476426eaa80bfde4e366da2fda7d52d67ad36f
Mouli Mohan N <moulimohann@Moulis-MacBook-Pro.local>
2025-10-30 13:21:40 +0530

    feat(bundle): add bundle analysis tools

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,12 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {createSerializer} = require('react-native-bundle-discovery');
 
 const defaultConfig = getDefaultConfig(__dirname);
+
+const mySerializer = createSerializer({
+  includeCode: true, // Set to true if you want to compare source/bundle code, but this will result in a larger report file.
+  projectRoot: __dirname, // In a monorepo, this should point to the monorepo root.
+});
 
 /**
  * Metro configuration
@@ -13,6 +19,9 @@ const config = {
   // Enable package exports so Metro can resolve packages using their modern "exports" field
   // (React 18/19 use `exports` and Metro may need this flag enabled for correct resolution).
   unstable_enablePackageExports: true,
+  },
+  serializer: {
+    customSerializer: mySerializer,
   },
 };
 
